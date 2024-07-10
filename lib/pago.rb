3d85259e88d5cc187e3e2424255d501cf69f8c92
Tim Matti <tmatti56@gmail.com>
2024-07-09 21:25:59 -0400

    rubocop

--- lib/pago.rb
+++ lib/pago.rb
@@ -16,17 +16,11 @@ class Pago
 
     case payment_method
     when :check
-      Rails.logger.info 'Processing check: ' +
-                        payment_details.fetch(:routing).to_s + '/' +
-                        payment_details.fetch(:account).to_s
+      Rails.logger.info "Processing check: #{payment_details.fetch(:routing)}/#{payment_details.fetch(:account)}"
     when :credit_card
-      Rails.logger.info 'Processing credit_card: ' +
-                        payment_details.fetch(:cc_num).to_s + '/' +
-                        payment_details.fetch(:expiration_month).to_s + '/' +
-                        payment_details.fetch(:expiration_year).to_s
+      Rails.logger.info "Processing credit_card: #{payment_details.fetch(:cc_num)}/#{payment_details.fetch(:expiration_month)}/#{payment_details.fetch(:expiration_year)}"
     when :po
-      Rails.logger.info 'Processing purchase order: ' +
-                        payment_details.fetch(:po_num).to_s
+      Rails.logger.info "Processing purchase order: #{payment_details.fetch(:po_num)}"
     else
       raise "Unknown payment_method #{payment_method}"
     end
